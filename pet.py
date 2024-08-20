0a727af0e8e1f952a38bf1b8655d139680c6653d
Hakeem <work.hakeem@gmail.com>
2024-08-21 00:13:42 +0800

    Additional to previous commit.

--- pet.py
+++ pet.py
@@ -1,20 +1,25 @@
 # The current working code is used to create a table to persistently store the time when it is run. To be used to measure time between pet visits
 # as a way to simulate the pets getting hungry over time without using an actual server.
     # TO-DO: 
-    # 1. Make it so that no new rows of lastvisitTime are created. I want it so that during each successful visit, lastvisitTime will be replaced by the 
-    # previous currentTime. Then I want to calculate the difference between the two and translate that into actually reducing the hunger bar based on
-    # how far apart the visits are. This will instill the need to come back. FOMO anyone?
-    # 2. Make basic display section in pet.html to show to mr willie.
-    # 3. Comm with shawn so that he also includes the pet number as a stat to record for each user. Explanation down below in the DEPENDENCY section.
-    # 3. Make currentTime be recorded. It will be the time recorded for every visit except the first.
-    # 4. Make the hunger bar.
-    # 5. Make unique pet ID system that will save to a different row in the table depending on the pet.
+    # DONE 1. Make it so that no new rows of lastvisitTime are created. I want it so that during each successful visit, lastvisitTime will be replaced by the 
+    # previous currentTime. 
+    # 2. To calculate the difference between the two and store it in a new column. To make the hunger loss cumulative with each visit.
+    # 3. New column or table to be added to make the hunger loss cumulative. Otherwise, people will keep logging in to prevent the hunger loss from 
+    # reducing. 
+    # 4. Food/feeding system and how it connects to the hunger loss mechanic. Resets the hunger bar to a certain level.
+    # 5. Make unique pet ID system that will save to a different table or row depending on the pet.
+    # 6. Make basic display section in pet.html to show to mr willie.
+    # 7. Comm with shawn so that he also includes the pet number as a stat to record for each user. Explanation down below in the DEPENDENCY section.
+    # DONE 8. Make currentTime be recorded. It will be the time recorded for every visit except the first.
+    # 9. Make the hunger bar.
+    # 
 
 import time, sqlite3
 
 petsOwned = 1 #temporary until there is a counter for pets owned for each user.
 lastvisitTime = (time.time(),) #apparently, this must be a tuple in order to replace any qmarks in the line below. #also, apparently not.
 currentTime = (time.time(),)
+petHunger = 100
 
 #------ACTUAL CODE------#
 #define connection and cursor
@@ -40,8 +45,10 @@ else:
         curs_obj.execute("UPDATE Time SET (currentTime) = (?)", currentTime)
         conn_obj.commit()
 
+
 curs_obj.execute("SELECT lastvisitTime,currentTime FROM Time ORDER BY lastvisitTime DESC LIMIT 1")
 print(curs_obj.fetchone()) #temporary, just to show that it still works when coding/testing.
+print(petHunger)
 conn_obj.close()
 
 #---DEPENDENCY ON OTHER SYSTEMS---#
